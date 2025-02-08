5196c80c40da820ee8b69395fc8cc43753822b7b
tirthake <tirthake@gmail.com>
2025-02-08 20:31:29 +0600

    Create reverse of xml.c
    reverse the xml into lmx using character variable

---
+++ reverse of xml.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char char1='x';
+char char2='m';
+char char3='l';
+
+printf("the reverse of %C%c%C is %c%c%c\n",char1,char2,char3,char3,char2,char1);
+    return 0;
+
+}//reverse character using char function
