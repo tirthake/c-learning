af1a9571bb8d1623ad6a568e89b4c11a2bd70dd6
imtirtha <tirthake@gmail.com>
2025-02-11 11:04:20 +0600

    Add files via upload

---
+++ sum of number 1 - 100.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,sum;
+    for
+
+    ( i=1; i<=100; i++)
+        sum+=i;// to add next value to previous value
+
+    {
+        printf("the sum of number 1-100 is:%d\n",sum);
+    }
+    return 0;
+}
