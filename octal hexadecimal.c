73cfece80e54c0dcce40297d3f36b84ff2b4f105
imtirtha <tirthake@gmail.com>
2025-02-17 09:44:46 +0600

    Add files via upload

---
+++ octal hexadecimal.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//hexa-octa,octa-hexa
+int main()
+{
+
+ /* int number;
+  printf("octal number=");
+  scanf("%o",&number);
+  printf("hexa-decimal number=%x",number);
+
+  */
+
+
+  int number;
+  printf("hexa-decimal number=");
+  scanf("%x",&number);
+  printf("octal number=%o",number);
+  getch();
+
+
+
+  getch();
+}//input uotput
