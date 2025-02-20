9375ba9e490a40c23d9844cce7a6df4541ff900b
imtirtha <tirthake@gmail.com>
2025-02-20 21:46:46 +0600

    Add files via upload
    write a code to sum of 5 number using array function

---
+++ array.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int main()
+{
+    //array declaration
+    int numbers[5]={10,20,30,40,50};// everything in once
+// when we use this value na dileo hobe
+
+    /*numbers[0]=20;
+    numbers[1]=30;
+    numbers[2]=40;
+    numbers[3]=50;
+    numbers[4]=60;//initialise*/
+
+    printf("%d\n",numbers[0]);
+    printf("%d\n",numbers[1]);
+    printf("%d\n",numbers[2]);
+    printf("%d\n",numbers[3]);
+    printf("%d\n",numbers[4]);
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
