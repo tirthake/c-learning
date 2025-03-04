7d8ddd26ac914f964a469ce4a6c49a265fc18455
TIRTHA DAS <tirthake@gmail.com>
2025-03-04 22:29:20 +0600

    Add files via upload

---
+++ go to.c
@@ -0,0 +1,17 @@
+// go to statement
+#include<stdio.h>
+int main()
+
+{
+    int i=1;
+print:// create level
+    printf("%d\t",i);// t used for tab
+    i++;
+
+    if(i<=7)
+goto print;// label calling here
+
+
+
+    return 0;
+}
